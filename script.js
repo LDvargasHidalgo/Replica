168452bcaa638c61a8f3348101c3c60a891092ef
mint <danielavargashidalgo@gmail.com>
2022-01-17 19:12:32 -0500

    navbar activo

---
+++ script.js
@@ -0,0 +1,12 @@
+let items = document.querySelectorAll(".item")
+
+items.forEach(item => {
+    item.addEventListener("click", (e) => {
+        items.forEach(item => {
+            item.classList = "item"
+        })
+        e.target.classList.toggle(("active"))
+    })
+})
+
+console.log("conectado")
